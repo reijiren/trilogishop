418711d06fed9601fe3c26b7139ce073fc9043cc
reijiren <rhefa.farizi@gmail.com>
2024-06-03 15:12:25 +0700

    initial commit

---
+++ src/main/java/com/trilogi/trilogishop/TrilogishopApplication.java
@@ -0,0 +1,13 @@
+package com.trilogi.trilogishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrilogishopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrilogishopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trilogi/trilogishop/TrilogishopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trilogi.trilogishop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrilogishopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
